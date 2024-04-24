6c0985dd804e38abe0a54c534baac209cb3ad1c8
Anusha Talagapu <anushatalagapu@gmail.com>
2024-04-24 12:52:47 +0530

    linked list

---
+++ l.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+    Node(int data1,Node*next1){
+       data1=data;
+       next1=next;
+    }
+
+};
